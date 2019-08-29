1b30300ebc1e4d9682eeb663fca82dedb759cd27
notuser <asdf@notmail.not>
2019-08-29 19:31:21 +0300

    Modified method wallpaper managing

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -7,7 +7,7 @@ setxkbmap -layout "us,ru" -model pc104 -variant "winkeys" -option "grp_led:scrol
 xset r rate 400 50 &
 
 # download and set wallpaper
-url_wp=https://images.wallpaperscraft.ru/image/tuman_doroga_derevia_135964_1920x1080.jpg
+url_wp=$(cat ~/.config/i3/url_wp)
 set_wp="feh --no-fehbg --bg-scale $(realpath ~/.config/i3/wp.jpg)"
 [ -f ~/.config/i3/wp.jpg ] && ($set_wp &) || ((echo "Downloading wallpaper" && wget -O ~/.config/i3/wp.jpg $url_wp && $set_wp) &)
 
