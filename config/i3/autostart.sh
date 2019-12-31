23bf6c0e6ca1ea8688b6ecb2df3719170d4e032e
notuser <asdf@notmail.not>
2019-12-31 18:27:03 +0300

    Some changes SUCK

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -7,7 +7,7 @@ setxkbmap -layout "us,ru" -model pc104 -variant "winkeys" -option "grp_led:scrol
 xset r rate 400 50 &
 
 # download and set wallpaper
-url_wp=$(cat ~/.config/i3/url_wp)
+url_wp=$(cat url_wp)
 set_wp="feh --no-fehbg --bg-scale $(realpath ~/.config/i3/wp.jpg)"
 [ -f ~/.config/i3/wp.jpg ] && ($set_wp &) || ((echo "Downloading wallpaper" && wget -O ~/.config/i3/wp.jpg $url_wp && $set_wp) &)
 
@@ -19,3 +19,5 @@ flameshot &
 
 # start clipboard tracking
 gpaste-client start &
+
+safeeyes &
