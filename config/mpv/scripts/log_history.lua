34764810e65033b7148d97964ba5f91112cfc8b9
UnkwUsr <ktoto2707043@gmail.com>
2022-05-05 17:32:36 +0300

    [mpv/scripts/log_history] Print title

--- config/mpv/scripts/log_history.lua
+++ config/mpv/scripts/log_history.lua
@@ -1,6 +1,12 @@
-local HISTFILE = mp.command_native({"expand-path", "~~home/"}) .. "/history";
-local logfile = io.open(HISTFILE, "a");
+local HISTFILE = mp.command_native({ "expand-path", "~~home/" }) .. "/history"
 
-mp.register_event('file-loaded', function ()
-    logfile:write(("%s\n"):format(mp.get_property("path")));
-end)
+function on_fileloaded()
+    local path = mp.get_property("path")
+    local title = mp.get_property("media-title")
+
+    local logfile = io.open(HISTFILE, "a")
+    logfile:write(("%s %s\n"):format(path, title))
+    logfile:close()
+end
+
+mp.register_event("file-loaded", on_fileloaded)
