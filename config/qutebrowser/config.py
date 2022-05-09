f13deb4b3881aca191f5607de0e8e4bcb535a2e9
UnkwUsr <ktoto2707043@gmail.com>
2022-05-09 15:04:02 +0300

    [quteb] Format code

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -6,8 +6,10 @@ config = config  # noqa: F821
 config.load_autoconfig(False)
 
 # improve performance
-c.qt.args = ["enable-gpu-rasterization", "enable-oop-rasterization",
-        "ignore-gpu-blacklist", "enable-accelerated-video-decode"]
+c.qt.args = [
+    "enable-gpu-rasterization", "enable-oop-rasterization",
+    "ignore-gpu-blacklist", "enable-accelerated-video-decode"
+]
 
 c.session.lazy_restore = True
 c.auto_save.session = True
@@ -24,13 +26,16 @@ c.content.register_protocol_handler = False
 
 c.content.blocking.method = "both"
 c.content.blocking.adblock.lists.clear()
-c.content.blocking.adblock.lists.append("https://easylist-downloads.adblockplus.org/advblock.txt")
-c.content.blocking.adblock.lists.append("https://easylist-downloads.adblockplus.org/cntblock.txt")
+c.content.blocking.adblock.lists.append(
+    "https://easylist-downloads.adblockplus.org/advblock.txt")
+c.content.blocking.adblock.lists.append(
+    "https://easylist-downloads.adblockplus.org/cntblock.txt")
 
 c.content.user_stylesheets.clear()
 c.content.user_stylesheets.append("~/.config/qutebrowser/styles.css")
 
-# see more parameters we can block at https://github.com/qutebrowser/qutebrowser/issues/3648
+# see more parameters we can block at
+# https://github.com/qutebrowser/qutebrowser/issues/3648
 config.set('content.javascript.enabled', False, 'stackoverflow.com')
 config.set('content.javascript.enabled', False, 'www.google.com/search*')
 # config.set('content.javascript.enabled', False, 'habr.com')
@@ -62,20 +67,25 @@ c.search.wrap = False
 
 c.spellcheck.languages = ["en-US", "ru-RU"]
 
-c.url.searchengines["DEFAULT"] = "https://html.duckduckgo.com/html?q={}"
-c.url.searchengines["g"] = "https://www.google.com/search?q={}"
-c.url.searchengines["a"] = "https://wiki.archlinux.org/index.php/{}"
-c.url.searchengines["yt"] = "https://" + redir.YOUTUBE_REDIR +"/results?search_query={}"
-c.url.searchengines["w"] = "https://en.wikipedia.org/wiki/{}"
-c.url.searchengines["wr"] = "https://ru.wikipedia.org/wiki/{}"
-c.url.searchengines["gi"] = "https://github.com/search?q={}"
-c.url.searchengines["aur"] = "https://aur.archlinux.org/packages/?K={}&SB=v&SO=d"
-c.url.searchengines["dc"] = "https://docs.rs/{}"
-c.url.searchengines["dr"] = "https://doc.rust-lang.org/std/index.html?search={}"
+c.url.searchengines = {
+    "DEFAULT": "https://html.duckduckgo.com/html?q={}",
+    "g": "https://www.google.com/search?q={}",
+    "a": "https://wiki.archlinux.org/index.php/{}",
+    "yt": "https://" + redir.YOUTUBE_REDIR + "/results?search_query={}",
+    "w": "https://en.wikipedia.org/wiki/{}",
+    "wr": "https://ru.wikipedia.org/wiki/{}",
+    "gi": "https://github.com/search?q={}",
+    "aur": "https://aur.archlinux.org/packages/?K={}&SB=v&SO=d",
+    "dc": "https://docs.rs/{}",
+    "dr": "https://doc.rust-lang.org/std/index.html?search={}"
+}
 
 c.completion.open_categories = ["quickmarks", "history"]
 
-c.editor.command = ["st", "-c", "st_win", "vim", "-f", "{file}", "-c", "normal {line}G{column0}l"]
+c.editor.command = [
+    "st", "-c", "st_win", "vim", "-f", "{file}", "-c",
+    "normal {line}G{column0}l"
+]
 
 config.bind("J", "tab-prev")
 config.bind("K", "tab-next")
@@ -88,14 +98,19 @@ config.bind("<Ctrl+shift+e>", "edit-command", mode='command')
 
 config.unbind('<Ctrl-v>', mode='normal')
 
-config.bind("<ESC>", "clear-keychain ;; search ;; fullscreen --leave ;; clear-messages")
+config.bind(
+    "<ESC>",
+    "clear-keychain ;; search ;; fullscreen --leave ;; clear-messages")
+
 
 # open in video player
 config.bind(",m", "spawn -vd mpv {url} --profile=quteb")
 config.bind(",M", "hint media spawn -vd mpv {hint-url} --profile=quteb")
 
 # enable/disable tor proxy
-config.bind("st", "set content.proxy socks://localhost:9050/ ;; message-info 'tor enabled'")
+config.bind(
+    "st",
+    "set content.proxy socks://localhost:9050/ ;; message-info 'tor enabled'")
 config.bind("sT", "set content.proxy system ;; message-info 'tor disabled'")
 
 # workaround to make youtube redirector work
--- config/qutebrowser/pyconfig/redirectors.py
+++ config/qutebrowser/pyconfig/redirectors.py
@@ -4,6 +4,7 @@ from qutebrowser.utils import message
 # choose one on https://api.invidious.io
 YOUTUBE_REDIR = "invidious.sethforprivacy.com"
 
+
 def try_redirects(info: qutebrowser.api.interceptor.Request):
     host = info.request_url.host()
     if host not in redirs:
@@ -21,22 +22,24 @@ def try_redirects(info: qutebrowser.api.interceptor.Request):
         info.block()
         pass
 
-redirs = {
-        "reddit.com": "teddit.net",
-        "www.reddit.com": "teddit.net",
-        "twitter.com": "nitter.net",
-        "imgur.com": "imgin.voidnet.tech",
 
-        # NOTE: to make youtube redirector work, you should clean service workers
-        "www.youtube.com": YOUTUBE_REDIR,
-        "youtube.com": YOUTUBE_REDIR,
-        "youtu.be": YOUTUBE_REDIR,
-        }
+redirs = {
+    "reddit.com": "teddit.net",
+    "www.reddit.com": "teddit.net",
+    "twitter.com": "nitter.net",
+    "imgur.com": "imgin.voidnet.tech",
+
+    # NOTE: to make youtube redirector work, you should clean service workers
+    "www.youtube.com": YOUTUBE_REDIR,
+    "youtube.com": YOUTUBE_REDIR,
+    "youtu.be": YOUTUBE_REDIR,
+}
 
 # remove previous registered interceptor (in case when run :config-source
 # command)
 if hasattr(qutebrowser.api, "redirector_intercept_func"):
-    qutebrowser.api.interceptor.interceptors._interceptors.remove(qutebrowser.api.redirector_intercept_func)
+    qutebrowser.api.interceptor.interceptors._interceptors.remove(
+        qutebrowser.api.redirector_intercept_func)
 
 qutebrowser.api.redirector_intercept_func = try_redirects
 qutebrowser.api.interceptor.register(try_redirects)
