d43496a402cb544ca02c705ee043b97a5677b3c3
UnkwUsr <ktoto2707043@gmail.com>
2022-06-25 18:48:18 +0300

    [mpv/scripts/log_history] Rewrite

--- config/mpv/scripts/log_history.lua
+++ config/mpv/scripts/log_history.lua
@@ -1,12 +1,34 @@
+-- "~/.config/mpv/history" by default
 local HISTFILE = mp.command_native({ "expand-path", "~~home/" }) .. "/history"
 
-function on_fileloaded()
-    local path = mp.get_property("path")
-    local title = mp.get_property("media-title")
+-- log format:
+-- <url> <artist_name_if_exist> - <track_title>
+-- example:
+-- https://someurl.fm/stream artist name - track title
+
+local function logline(path, title)
+    local artist = mp.get_property("metadata/by-key/artist")
+    if artist ~= nil then
+        title = artist .. " - " .. title
+    end
 
     local logfile = io.open(HISTFILE, "a")
-    logfile:write(("%s %s\n"):format(path, title))
-    logfile:close()
+    if logfile ~= nil then
+        logfile:write(("%s %s\n"):format(path, title))
+        logfile:close()
+    else
+        mp.osd_message("[log_history] Can't open file " .. HISTFILE)
+    end
 end
 
-mp.register_event("file-loaded", on_fileloaded)
+local function on_title_change(_, new_title)
+    local filename = mp.get_property("filename")
+    if new_title == filename then
+        return
+    end
+
+    local path = mp.get_property("path")
+    logline(path, new_title)
+end
+
+mp.observe_property("media-title", "string", on_title_change)
