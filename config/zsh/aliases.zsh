a2244bed8b21915726cf561125569215f29e5a17
UnkwUsr <ktoto2707043@gmail.com>
2022-07-16 13:52:01 +0300

    [zsh/aliases] Refactor

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -1,30 +1,39 @@
-# make colored and human-readable
-alias ls='ls --color=auto -h --group-directories-first'
-alias du='du -h'
-alias df='df -h -x tmpfs -x devtmpfs --output=source,size,avail,target'
+#### just settings for some programs
+
+# color and human-readable
 alias pacman='pacman --color=auto'
 alias dmesg='dmesg --color=auto'
 alias diff='diff --color=auto'
+alias ls='ls --color=auto -h --group-directories-first'
+alias du='du -h'
+alias df='df -h -x tmpfs -x devtmpfs --output=source,size,avail,target'
 alias ffmpeg='ffmpeg -hide_banner'
 alias ffplay='ffplay -hide_banner'
 alias ffprobe='ffprobe -hide_banner'
-alias cal='cal -m'
-# make verbose
+
+# verbose
 alias cp='cp -v'
 alias mv='mv -v'
 alias rm='rm -v'
 alias chmod='chmod -v'
-# set rg case-sensitivity method 'smart'
+
+# case-sensitivity method 'smart'
 alias rg='rg -S'
+# start week from Monday
+alias cal='cal -m'
+
+
+#### new commands
 
 # ls shortcuts
 alias ll='ls -l'
 alias la='ls -lA'
-# vim with gv plugin
-alias gv='vim +:GV'
-alias gva='vim +":GV --all"'
 
 # typos
 alias l='ll'
 alias lll='ll'
 alias dc='cd'
+
+# vim with gv plugin
+alias gv='vim +:GV'
+alias gva='vim +":GV --all"'
