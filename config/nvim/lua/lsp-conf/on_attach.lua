bb26ec92d029e3a198d7ea561cbb593e43fd9dc3
UnkwUsr <ktoto2707043@gmail.com>
2022-10-05 20:55:45 +0300

    [vim/lsp/on_attach] Refactor

--- config/nvim/lua/lsp-conf/on_attach.lua
+++ config/nvim/lua/lsp-conf/on_attach.lua
@@ -1,3 +1,38 @@
+local function map_keys(client, bufnr)
+    local function keymap(mode, key, cmd)
+        local opts = { noremap = true, silent = true }
+        vim.api.nvim_buf_set_keymap(bufnr, mode, key, cmd, opts)
+    end
+
+    -- stylua: ignore start
+    keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>")
+    keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>")
+    keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>")
+    keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+    keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+    keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>")
+    keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>")
+    keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>")
+    keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+    keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+    keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+    keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>")
+    keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+    keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+    keymap("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>")
+    keymap("n", "<space>c", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+    keymap("v", "<space>c", "<cmd>lua vim.lsp.buf.range_code_action()<CR>")
+    -- stylua: ignore end
+
+    if client.server_capabilities.documentFormattingProvider then
+        -- stylua: ignore
+        keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.format { async = true }<CR>")
+    elseif client.server_capabilities.documentRangeFormattingProvider then
+        -- stylua: ignore
+        keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.format { async = true }<CR>")
+    end
+end
+
 local on_attach = function(client, bufnr)
     -- disable sumneko_lua formatting as we use stylua via null-ls
     if client.name == "sumneko_lua" then
@@ -5,35 +40,9 @@ local on_attach = function(client, bufnr)
         client.server_capabilities.documentRangeFormattingProvider = false
     end
 
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    local opts = { noremap=true, silent=true }
-    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-    buf_set_keymap('n', '<space>c', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('v', '<space>c', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
-
-    if client.server_capabilities.documentFormattingProvider then
-        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.format { async = true }<CR>", opts)
-    elseif client.server_capabilities.documentRangeFormattingProvider then
-        buf_set_keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.format { async = true }<CR>", opts)
-    end
+    map_keys(client, bufnr)
 end
 
 return on_attach
