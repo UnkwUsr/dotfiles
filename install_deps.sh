b2fe9730f5cfda145a12664df26f831f6093467e
UnkwUsr <ktoto2707043@gmail.com>
2022-10-16 14:07:28 +0300

    [mpv] Install scripts to crop and cut (slice)

--- install_deps.sh
+++ install_deps.sh
@@ -4,6 +4,11 @@
 wget -nv -P ~/.config/vifm/ -x -nH --cut-dirs=4 \
     'https://raw.githubusercontent.com/vifm/vifm/f716368b053dcdb6019251a53d87183c0045ffc2/data/plugins/viewcolumn/init.lua'
 
+# mpv plugins
+wget -nv -P ~/.config/mpv/ -x -nH --cut-dirs=3 \
+    'https://raw.githubusercontent.com/occivink/mpv-scripts/af360f332897dda907644480f785336bc93facf1/scripts/crop.lua' \
+    'https://raw.githubusercontent.com/occivink/mpv-scripts/af360f332897dda907644480f785336bc93facf1/scripts/encode.lua'
+
 # lldb plugins
 wget -nv -O ~/.config/lldb_write.py \
     'https://raw.githubusercontent.com/4iar/lldb-write/1baf24f9a524bbb967708307059d54b39943a9c3/write.py'
