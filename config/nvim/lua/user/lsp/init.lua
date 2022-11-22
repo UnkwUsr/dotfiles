7a78cb6c5c69194ee89c5b2fc4d1c43955be4a6a
UnkwUsr <ktoto2707043@gmail.com>
2022-11-22 19:38:04 +0300

    [vim/lsp] Update due to cmp plugin update

--- config/nvim/lua/user/lsp/init.lua
+++ config/nvim/lua/user/lsp/init.lua
@@ -1,6 +1,4 @@
-local capabilities = require("cmp_nvim_lsp").update_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
-)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local servers = require("user.lsp.servers")
 for server_name, settings in pairs(servers) do
