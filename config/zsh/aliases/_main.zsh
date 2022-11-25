97e65190285d30a142870dd1e645c73423edc714
UnkwUsr <ktoto2707043@gmail.com>
2022-11-26 02:59:46 +0300

    [zsh/aliases] add all txts stuff (dtm/zk/diary/remind/inbox)

--- config/zsh/aliases/_main.zsh
+++ config/zsh/aliases/_main.zsh
@@ -2,3 +2,4 @@ source ~/.config/zsh/aliases/default_options.zsh
 source ~/.config/zsh/aliases/custom_commands.zsh
 source ~/.config/zsh/aliases/pacman.zsh
 source ~/.config/zsh/aliases/noise.zsh
+source ~/.config/zsh/aliases/txts.zsh
---
+++ config/zsh/aliases/txts.zsh
@@ -0,0 +1,80 @@
+#### txts aliases (note taking, planning, etc.)
+
+# aliases naming legend (applies to dtm/plans, zk, and few other):
+# .i - Index (open index file)
+# .s - Search (search through base with fzf)
+# .d - Directory (cd into)
+# . - just opens main entry point
+#
+# example:
+# d - open main dtm entry point
+# di - open dtm index
+# ds - dtm search
+# dtmd - cd into dtm directory (this is exception, but suffix `d` still here)
+
+
+### dtm - plans, tasks, thoughts/ideas
+# (historically it named "dtm", but now most binds starts with just "d")
+
+alias di="cd ~/txts/dtm && vim 00_index.md && cd - > /dev/null"
+alias dtmd="cd ~/txts/dtm && pwd"
+alias ds="cd ~/txts/dtm && zk edit -i -s modified"
+# inbox, part of dtm, most often entry point. Playground for thoughts, etc.
+alias d="cd ~/txts/dtm && vim plans.md && cd - > /dev/null"
+
+
+### semi-sorted inbox repo (aka dtm pre-staging area)
+
+alias dwd="cd ~/txts/dtm/inbox"
+# entry point, aka "not in priority"
+alias dw="cd ~/txts/dtm && vim inbox/0spn.md"
+# saved notes from i3 bind
+alias da="cd ~/txts/dtm/inbox/ && vim +':norm G' 00_inboxed.md && cd - > /dev/null"
+# saved links from browser
+alias db="cd ~/txts/dtm/inbox && vim quteb_saved_urls.md"
+
+# add to inbox right from cli. Yes
+ibn() { echo "* from ibn: $*" >> ~/txts/dtm/inbox/00_inboxed.md }
+
+# buy list
+alias bb="cd ~/txts/synced-inbox && vim buy.md && cd - > /dev/null"
+
+
+### zk - zettel notes
+
+alias z="cd ~/txts/zk2 && vim 00_INDEX.md"
+alias zi="cd ~/txts/zk2 && vim inbox/00_INBOX.md"
+alias zs="cd ~/txts/zk2 && zk edit -i -s modified"
+alias zd="cd ~/txts/zk2 && pwd"
+
+
+### remind/calendar ("tasks on time")
+
+# view today
+alias rem='rem -m -@'
+# view current week
+alias rem1='clear; rem -cl+1 -w,3,0'
+# view current and next week
+alias rem2='clear; rem -cl+2 -w,3,0'
+# view current and previous week
+alias rem2p='clear; rem -cl+2 -w,3,0 $(date -d"-7 day" +"%d %b %Y")'
+# view current and 2 next weeks
+alias rem3='clear; rem -cl+3 -w,3,0'
+# view whole year
+alias remy='rem -cl+53 -w150,3,0 | less -r'
+
+# mark as done repeatable reminds
+alias remd='~/.reminders/mark_done.sh'
+# edit reminds (kind of entry point for reminds)
+# '+args\ %' hack to suppress 'E173: 1 more file to edit' warning
+alias reme='cd ~/.reminders && vim +args\ % main.rem repeats.rem'
+
+
+### diary
+
+alias dtd="cd ~/txts/diary && pwd"
+# open today diary file
+# -3 hours offset for the case when now is "2022-01-02 01:15" and I want to
+# write diary for "2022-01-01"
+alias dt='cd ~/txts/diary/$(date +"%m" -d "-3 hours") && \
+    vim $(date +"%Y_%m_%d" -d "-3 hours") && cd - > /dev/null'
