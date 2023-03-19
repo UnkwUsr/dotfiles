f121bedd91e6bf2f564eeb3d52715203f031b524
UnkwUsr <ktoto2707043@gmail.com>
2023-03-19 19:11:02 +0300

    [zsh/aliases/custom_commands] Refactor

--- config/zsh/aliases/custom_commands.zsh
+++ config/zsh/aliases/custom_commands.zsh
@@ -4,20 +4,19 @@
 alias ll='ls -l'
 alias la='ls -lA'
 
-# ripgrep shortcuts
-alias rgf='rg --count'
-
 # typos
 alias l='ll'
 alias lll='ll'
 alias dc='cd'
 
-# open file from dotfiles directory
-alias sd='cd ~/.dotfiles && fzf \
-    --preview="bat {} --style changes --decorations always --color always" \
-    | xargs -r vim; cd - > /dev/null'
-# cd to directory of the file in dotfiles
-alias sdd='cd ~/.dotfiles && cd $(dirname $(fzf)) && pwd'
+# file browser
+alias fm='vifm'
+
+# show files with matches
+alias rgf='rg --count'
+
+
+#### vim+git
 
 # vim with gv plugin, shows git log
 gv() {
@@ -28,18 +27,28 @@ gva() { gv "--all $*" }
 compdef -e 'words[1]=(git log); service=git; (( CURRENT+=1 )); _git' gv gva
 # Source: https://stackoverflow.com/questions/27226716/custom-zsh-completion-for-a-function-based-on-default-arguments
 
-# file browser
-alias fm='vifm'
+# vim fzf over git changed files
+alias vfl='vim +":GFiles?"'
+
+
+#### vim
 
 # open vim recent files history (with fzf)
 alias vh='vim +":History"'
 # open vim recent file
 alias vo='vim +":exe \"normal \<C-o>\""'
-# vim open files selected via fzf
+
+# open vim with files selected via fzf
 alias vf='vim $(fzf -m)'
-# vim fzf over git changed files
-alias vfl='vim +":GFiles?"'
 # fd search for files and open them in vim
-vfd() {
-    vim $(fd -tf "$@")
-}
+vfd() { vim $(fd -tf "$@") }
+
+
+#### dotfiles managing
+
+# open file from dotfiles directory
+alias sd='cd ~/.dotfiles && fzf \
+    --preview="bat {} --style changes --decorations always --color always" \
+    | xargs -r vim; cd - > /dev/null'
+# cd to directory of the file in dotfiles
+alias sdd='cd ~/.dotfiles && cd $(dirname $(fzf)) && pwd'
