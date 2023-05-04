1ee778517874cb67f3146185203c0f13c7899990
UnkwUsr <ktoto2707043@gmail.com>
2023-05-04 19:21:40 +0300

    [zsh/aliases] Add aliases for noise producing

--- config/zsh/aliases/_main.zsh
+++ config/zsh/aliases/_main.zsh
@@ -1,3 +1,4 @@
 source ~/.config/zsh/aliases/default_options.zsh
 source ~/.config/zsh/aliases/custom_commands.zsh
 source ~/.config/zsh/aliases/pacman.zsh
+source ~/.config/zsh/aliases/noise.zsh
---
+++ config/zsh/aliases/noise.zsh
@@ -0,0 +1,18 @@
+#### mpv for noise performance
+
+# play in mpv with noise profile
+mpz() { clear; exec mpv --profile=noise "$1" }
+# play all tracks each in separate tmux window
+mpt() {
+    for file in "$@"
+    do
+        tmux new-window -d "mpv --profile=noise \"$file\""
+    done
+}
+# play N times all mentioned tracks (each in separate tmux window)
+mpn() {
+    for i in $(seq $1)
+    do
+        mpt "${@:2}"
+    done
+}
