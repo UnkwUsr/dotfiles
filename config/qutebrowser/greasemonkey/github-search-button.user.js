f074c7620b776b9e25f243698494b58a451c8a0c
UnkwUsr <ktoto2707043@gmail.com>
2023-07-12 02:16:55 +0300

    [quteb/greasemonkey] Add github search button

---
+++ config/qutebrowser/greasemonkey/github-search-button.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name        github search button
+// @description Add search field to be used with `gi` bind
+// @namespace   *.github.com
+// @include     *://github.com/*
+// @version     1.0
+// @license     MIT
+// @run-at      document-idle
+// @grant       none
+// ==/UserScript==
+
+function myfix() {
+    let me = document.getElementsByClassName("js-site-search-type-field")[0];
+    if (!me) {
+        return;
+    }
+
+    // removes type="hidden" attribute
+    me.removeAttribute("type");
+
+    me.style = "\
+    width: 0px;\
+    height: 0;\
+    margin-left: 11px;\
+    position: absolute;";
+
+    me.onfocus = (_event) => {
+        let btn = document.getElementsByClassName("header-search-button")[0];
+        btn.click();
+    };
+}
+
+function observer_callback(_mutationList, _observer) {
+    myfix();
+}
+
+const observer = new MutationObserver(observer_callback);
+observer.observe(document.body, {
+    childList: true,
+});
+
+myfix();
