440680e70a94b00188add078c289506a8702d58d
UnkwUsr <ktoto2707043@gmail.com>
2023-11-28 14:22:40 +0300

    [install_deps] mpv: plugin to fix bindings in russian layout

--- install_deps.sh
+++ install_deps.sh
@@ -7,7 +7,8 @@ wget -nv -P ~/.config/vifm/ -x -nH --cut-dirs=4 \
 # mpv plugins
 wget -nv -P ~/.config/mpv/ -x -nH --cut-dirs=3 \
     'https://raw.githubusercontent.com/occivink/mpv-scripts/af360f332897dda907644480f785336bc93facf1/scripts/crop.lua' \
-    'https://raw.githubusercontent.com/occivink/mpv-scripts/af360f332897dda907644480f785336bc93facf1/scripts/encode.lua'
+    'https://raw.githubusercontent.com/occivink/mpv-scripts/af360f332897dda907644480f785336bc93facf1/scripts/encode.lua' \
+    'https://raw.githubusercontent.com/zenwarr/mpv-config/2a85d058403e67053eb3a70d1159718d884ec7a1/scripts/russian-layout-bindings.lua'
 wget -nv -P ~/.config/mpv/scripts/ -x -nH --cut-dirs=3 \
     'https://raw.githubusercontent.com/viniciusbm/mpv-cycle-through-existing/738a79a450b53edcd254d6e6ab20323fbce1cf8f/cycle-through-existing.lua'
 
