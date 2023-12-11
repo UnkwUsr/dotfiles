8e93d343d9850b4793a38c7ceae9c12bc521d0e0
UnkwUsr <ktoto2707043@gmail.com>
2023-12-11 13:21:52 +0300

    [zsh/aliases/custom_commands] add pandoc helpers

--- config/zsh/aliases/custom_commands.zsh
+++ config/zsh/aliases/custom_commands.zsh
@@ -120,6 +120,14 @@ aas() {
     echo "$imgfilename"
 }
 
+# pandoc with my defaults config
+alias mepan="pandoc -d ~/.config/pandoc/me.yaml"
+# mepan and open in zathura
+zpan() {
+    name="/tmp/rendered_$1_$(date +%s).pdf"
+    mepan "$1" -o "$name" && zathura "$name" & disown
+}
+
 # open lisp repl
 alias sbcl="rlwrap sbcl --noinform"
 # execute lisp script
