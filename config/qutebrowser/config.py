d60defe342c3e635c1cd8ef977f586149191ce21
UnkwUsr <ktoto2707043@gmail.com>
2023-12-14 13:08:56 +0300

    [quteb] bind to strip url parameters

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -103,6 +103,9 @@ config.bind("9gm", "tab-move -1")
 # copy url and title formatted
 config.bind("ym", "yank inline '* {title}: {url}'")
 
+# strip url parameters
+config.bind("gs", "navigate strip")
+
 config.bind("<Ctrl+w>", "rl-backward-kill-word", mode='command')
 config.bind("<Alt+Backspace>", "rl-rubout ' '", mode='command')
 config.bind("<Ctrl+shift+e>", "cmd-edit", mode='command')
