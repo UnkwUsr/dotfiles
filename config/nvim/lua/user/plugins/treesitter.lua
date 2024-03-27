0893acd3fd3ab0188d2db425fbf95ff625036b53
UnkwUsr <ktoto2707043@gmail.com>
2024-03-27 23:03:47 +0300

    [nvim/plugins/treesitter] disable auto installed parsers

--- config/nvim/lua/user/plugins/treesitter.lua
+++ config/nvim/lua/user/plugins/treesitter.lua
@@ -1,11 +1,17 @@
+-- they are installed by default, disabling them
+local disabled_parsers =
+    { "markdown", "markdown_inline", "bash", "c", "python", "vimdoc" }
+
 require("nvim-treesitter.configs").setup({
     ensure_installed = { "comment", "regex" },
 
     highlight = {
         enable = true,
+        disable = disabled_parsers,
     },
     indent = {
         enable = true,
+        disable = disabled_parsers,
     },
 
     refactor = {
