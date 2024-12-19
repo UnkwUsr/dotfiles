cfab88ee6be5cf965524e98b97185833b96cc650
UnkwUsr <ktoto2707043@gmail.com>
2024-12-20 00:48:10 +0300

    [aconfmgr/70-systemd] disable vmware-networks

--- aconfmgr/70-systemd.sh
+++ aconfmgr/70-systemd.sh
@@ -8,7 +8,7 @@ CreateLink /etc/systemd/user/sockets.target.wants/pipewire.socket /usr/lib/syste
 CreateLink /etc/systemd/user/sockets.target.wants/pulseaudio.socket /usr/lib/systemd/user/pulseaudio.socket
 ### my
 CreateLink /etc/systemd/system/multi-user.target.wants/vnstat.service /usr/lib/systemd/system/vnstat.service
-CreateLink /etc/systemd/system/paths.target.wants/vmware-networks.path /usr/lib/systemd/system/vmware-networks.path
+# CreateLink /etc/systemd/system/paths.target.wants/vmware-networks.path /usr/lib/systemd/system/vmware-networks.path
 CreateLink /etc/systemd/system/sockets.target.wants/docker.socket /usr/lib/systemd/system/docker.socket
 CreateLink /etc/systemd/system/multi-user.target.wants/dnscrypt-proxy.service /usr/lib/systemd/system/dnscrypt-proxy.service
 ### my custom units
