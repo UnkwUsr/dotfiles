0846ffe895c57a4601e29e788da788a43cf849b7
UnkwUsr <ktoto2707043@gmail.com>
2025-01-04 03:07:42 +0300

    [aconfmgr/70-patches] use mold linker

--- aconfmgr/70-patches.sh
+++ aconfmgr/70-patches.sh
@@ -6,9 +6,10 @@ ApplyPatch neovim /etc/xdg/nvim/sysinit.vim ./patches/neovim_sysinit.vim.diff
 ApplyPatch pam /etc/security/faillock.conf ./patches/pam_faillock.conf.diff
 ApplyPatch fuse-common /etc/fuse.conf ./patches/fuse-common_fuse.conf.diff
 ApplyPatch filesystem /etc/hosts ./patches/filesystem_hosts.diff
-ApplyPatch pacman /etc/makepkg.conf ./patches/pacman_makepkg.conf.diff
 ApplyPatch pam /etc/environment ./patches/pam_environment.diff
 ApplyPatch pacman /etc/pacman.conf ./patches/pacman_pacman.conf.diff
+ApplyPatch pacman /etc/makepkg.conf ./patches/pacman_makepkg.conf.diff
+ApplyPatch pacman /etc/makepkg.conf.d/rust.conf ./patches/pacman_rust.conf.diff
 ApplyPatch sudo /etc/sudoers ./patches/sudo_sudoers.diff
 ApplyPatch pambase /etc/pam.d/system-auth ./patches/pambase_system-auth.diff
 ApplyPatch dnscrypt-proxy /etc/dnscrypt-proxy/dnscrypt-proxy.toml ./patches/dnscrypt-proxy_dnscrypt-proxy.toml.diff
