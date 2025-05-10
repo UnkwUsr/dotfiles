edf94e199376518251b90344f6419b25d2857b46
UnkwUsr <ktoto2707043@gmail.com>
2025-05-10 10:20:55 +0300

    [bin/cpfiles] trim sequence number in filenames of music

--- bin/cpfiles.sh
+++ bin/cpfiles.sh
@@ -2,6 +2,16 @@
 # Source: https://superuser.com/a/1784430/1660523
 {
 for i in "$@"; do
-    echo -en "file://""$(realpath "${i}")""\n"
+    i="$(realpath -s "$i")"
+
+    # hack to trim sequence number in filenames of music and which is symlink
+    if [[ "$PWD" == *"Files/annexes/music"* ]] && [ -h "$i" ]; then
+        mkdir -p /tmp/cpfiles-messings
+        newi="/tmp/cpfiles-messings/$(basename "$i" | cut -d' ' -f2-)"
+        ln -sf "$i" "$newi"
+        i="$newi"
+    fi
+
+    echo -en "file://${i}\n"
 done
 } | xclip -i -sel c -rmlastnl -t text/uri-list
