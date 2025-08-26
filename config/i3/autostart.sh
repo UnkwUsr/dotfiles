c6b82b64d6db45ad0b2112b4bf6415ff9202ca67
UnkwUsr <ktoto2707043@gmail.com>
2025-08-26 12:05:46 +0300

    [i3/autostart] remove background as migrated to sway

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -5,10 +5,6 @@
 xset s 1200 0
 xset dpms 1200 0 0
 
-# set random wallpaper
-# feh --no-fehbg --bg-scale "$(fd -tf . ~/.config/i3/wallpapers/ | shuf | head -n1)"
-feh --no-fehbg --bg-scale ~/.config/i3/wallpapers/04_Miku_v2.png
-
 # notifications daemon
 dunst &
 
