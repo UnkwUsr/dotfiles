acb6870c02c06281004df13c90ec35855d422ecc
UnkwUsr <ktoto2707043@gmail.com>
2025-12-13 17:57:51 +0300

    [bin/auto_gather_inbox] get rid of annoying notifies

--- bin/auto_gather_inbox.sh
+++ bin/auto_gather_inbox.sh
@@ -2,12 +2,9 @@
 GATHER_REPEAT_INTERVAL="8h"
 GATHER_INBOX_LOG="/tmp/gather_inbox.sh.log"
 (sleep 5m && while true; do
-    notify-send -t 1000 "Running gather-inbox"
     echo >> "$GATHER_INBOX_LOG"
-    if gather-inbox.sh >> "$GATHER_INBOX_LOG" 2>&1; then
-        notify-send -t 1000 "gather-inbox done succesfully"
-    else
-        notify-send -u critical "gather-inbox done with error"
+    if ! gather-inbox.sh >> "$GATHER_INBOX_LOG" 2>&1; then
+        echo "gather-inbox done with error" >> ~/error-in-auto-gather-inbox
     fi
     sleep $GATHER_REPEAT_INTERVAL
 done)
